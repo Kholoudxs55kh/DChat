2eeae0e38b0390cf8c739e9561a6f6ce420855c7
kholoudxs55kh <kfattem@gmail.com>
2025-02-09 23:48:03 +0200

    Add data fetching functionality and update UI in App componentInitial project setup with Django, REST framework, and Swagger

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const data = async () => {
+    try {
+      const response = await fetch('http://192.168.1.19:3000/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const json = await response.json();
+      console.log(json)
+      setText(json.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setText('Failed to fetch data');
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>بَك</Text>
+      <Button title="Get Data" onPress={data} />
+      <Text>{text}</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
